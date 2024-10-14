6315f5b69093802efa19ace8d25c37cdf0bac615
Jakiik <83412140+Jakiik@users.noreply.github.com>
2024-10-14 19:49:25 +0700

    Added my project

--- app/components/Posts/ViewUserButton.tsx
+++ app/components/Posts/ViewUserButton.tsx
@@ -4,13 +4,20 @@ interface ViewUserButton {
     userId:number
 }
 
-const ViewUserButton: React.FC<ViewUserButtonProps> = ({userId}) => {
-    const handleClick = () => alert(`user id: ${userId}`);
-    return (
-        <>
-            <button onClick={handleClick}>Lihat User</button>
-        </>
-    );
+const ViewUserButton: React.FC<ViewUserButtonProps> = ({ userId }) => {
+  const handleClick = () => {
+    if (userId > 100) {
+      alert('Info selanjutnya bisa dilihat di website KAI');
+    } else {
+      alert('Info selanjutnya bisa dilihat di website KAI');
+    }
+  };
+  
+  return (
+    <>
+      <button onClick={handleClick}>Info selanjutnya</button>
+    </>
+  );
 };
 
-export default ViewUserButton
+export default ViewUserButton;
--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -1,36 +1,43 @@
+import KeretaData from "../../public/data/KeretaApi.json";
 import CardList from "../components/Posts/CardList";
 import ViewUserButton from "../components/Posts/ViewUserButton";
 
-const base_url = "https://jsonplaceholder.typicode.com/posts";
-
-interface Iposts {
-  userId: number;
-  id: number;
-  title: string;
-  body: string;
+interface Kereta {
+  IdLoko: number;
+  Nama: string;
+  Rute: string;
+  Lokomotif: string;
+  Daya: string;
+  Tiket: number;
+  Tipe: string;
+  KecepatanMaks: string;
+  BahanBakar: string;
+  Pemilik: string;
 }
 
-const Posts = async () => {
-  const response = await fetch(base_url, {
-    
-  });
-  const posts: Iposts[] = await response.json();
+const KeretaList = () => {
+  const Keretas: Kereta[] = KeretaData["Kereta Api"];
+  
   return (
     <>
-        <p>{new Date().toLocaleTimeString()}</p>
-      <h1 className="text-fuchsia-500">Posting Page</h1>
-      {posts.map((post) => {
-        return (
-          <CardList key={post.id}>
-            <p>{post.id}</p>
-            <i>{post.title}</i>
-            <p>{post.body}</p>
-            <ViewUserButton userId={post.userId} />
-          </CardList>
-        );
-      })}
+      <p>{new Date().toLocaleTimeString()}</p>
+      <h1 className="text-fuchsia-500">POSTINGAN PAGE</h1>
+      {Keretas.map((Kereta) => (
+        <CardList key={Kereta.IdLoko}>
+          <p>{Kereta.Nama}</p>
+          <i>Rute : {Kereta.Rute}</i>
+          <p>Lokomotif : {Kereta.Lokomotif}</p>
+          <p>Daya : {Kereta.Daya} </p>
+          <p>Harga Tiket : {Kereta.Tiket}</p>
+          <p>Tipe : {Kereta.Tipe} </p>
+          <p>Kecepatan Maksimal : {Kereta.KecepatanMaks}</p>
+          <p>Bahan Bakar : {Kereta.BahanBakar}</p>
+          <p>Pemilik : {Kereta.Pemilik}</p>
+          <ViewUserButton userId={Kereta} />
+        </CardList>
+      ))}
     </>
   );
 };
 
-export default Posts;
+export default KeretaList;
